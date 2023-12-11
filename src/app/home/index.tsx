6e94cc73a51371c8915445c42b1af71ff8e0ca50
이현재 (whale) <kj109888@gmail.com>
2023-12-12 01:27:31 +0900

    Update index.tsx

--- src/app/home/index.tsx
+++ src/app/home/index.tsx
@@ -1,7 +1,7 @@
 export const Home = () => {
   return (
     <div>
-      <h1 className="bg-red-50 p-2 text-rose-950">This is Home Page...!!!</h1>
+      <h1 className="bg-red-50 p-2 text-rose-950">This is Home Page...!</h1>
     </div>
   );
 };
