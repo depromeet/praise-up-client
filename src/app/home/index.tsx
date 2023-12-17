6ebb1da8e5a28dd518c2e21b72483a48846e40d2
Chaemin-L <chaem12@kookmin.ac.kr>
2023-12-18 02:38:52 +0900

    Chaemin-L | style: font 적용 및 디자인 토큰 그룹화

--- src/app/home/index.tsx
+++ src/app/home/index.tsx
@@ -2,6 +2,16 @@ export const Home = () => {
   return (
     <div>
       <h1 className="bg-red-50 p-2 text-rose-950">This is Home Page...233</h1>
+      <div className="bg-primary-400">asdf</div>
+      <img
+        src="https://www.urbanbrush.net/web/wp-content/uploads/edd/2022/11/urbanbrush-20221108214712319041.jpg"
+        className="ism rounded"
+      />
+      <p className="h1">
+        asdfasdfasdfasdf
+        <br />
+        asdfasdf
+      </p>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,22 +7,8 @@ export default {
         xs: "480px",
         "3xl": "1920px",
       },
-      width: {
-        /* Image */
-        ixs: "152px",
-        ism: "200px",
-        imd: "240px",
-        ilg: "284px",
-      },
-      height: {
-        /* Image */
-        ixs: "152px",
-        ism: "200px",
-        imd: "72px",
-        ilg: "284px",
-      },
       colors: {
-        "blue-gray": {
+        primary: {
           100: "#F9F9FA",
           200: "#F2F3F4",
           300: "#E8E9EA",
@@ -34,28 +20,9 @@ export default {
         },
       },
       borderRadius: {
-        sm: "8px",
+        DEFAULT: "8px",
         md: "12px",
         lg: "16px",
-      },
-      fontSize: {
-        hd1: "32px", // heading
-        hd2: "20px",
-        hd3: "18px",
-        bd1: "16px", // body
-        bd2: "14px",
-        cap: "12px", // caption
-      },
-      lineHeight: {
-        hd1: "38px", // heading
-        hd2: "26px",
-        hd3: "24px",
-        bd1: "22px",
-        "bd1-l": "24px", // body1 long
-        bd2: "20px",
-        "bd2-l": "22px", // body2 long
-        cap: "18px",
-        "cap-l": "20px", // caption long
       },
     },
   },
