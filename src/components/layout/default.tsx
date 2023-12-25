d301212c3f9519b06557aa09bc0d4d24e2579068
2-NOW <kj109888@gmail.com>
2023-12-26 00:19:01 +0900

    chore: format by lint

--- src/components/layout/default.tsx
+++ src/components/layout/default.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 export const DefaultLayout = ({ appbar, children }: Props) => {
   return (
-    <div className='"max-w-[480px]"'>
+    <div className="max-w-[480px]">
       {/* need app bar variants */}
       {appbar ?? <div className="h-16" />}
       <div className="px-5 pt-4">{children}</div>
