7421e732a56945f5e966ce3cb654c7fcaa65d9c9
2-NOW <kj109888@gmail.com>
2023-12-26 00:19:01 +0900

    chore: format by lint

--- src/app/home/index.tsx
+++ src/app/home/index.tsx
@@ -3,7 +3,7 @@ import { DefaultLayout } from "@/components/layout/default";
 export const Home = () => {
   return (
     <DefaultLayout>
-      <div className="bg-red-500 w-full h-20">Hello</div>
+      <div className="h-20 w-full bg-red-500">Hello</div>
     </DefaultLayout>
   );
 };
