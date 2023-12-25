be6b2770439268cfe7460ab5b7081897b39df96f
2-NOW <kj109888@gmail.com>
2023-12-26 00:15:51 +0900

    feat: add default layout

--- src/app/home/index.tsx
+++ src/app/home/index.tsx
@@ -1,7 +1,9 @@
+import { DefaultLayout } from "@/components/layout/default";
+
 export const Home = () => {
   return (
-    <div>
-      <h1 className="bg-red-50 p-2 text-rose-950">This is Home Page...!</h1>
-    </div>
+    <DefaultLayout>
+      <div className="bg-red-500 w-full h-20">Hello</div>
+    </DefaultLayout>
   );
 };
---
+++ src/components/layout/default.tsx
@@ -0,0 +1,15 @@
+import { PropsWithChildren, ReactNode } from "react";
+
+type Props = {
+  appbar?: ReactNode;
+} & PropsWithChildren;
+
+export const DefaultLayout = ({ appbar, children }: Props) => {
+  return (
+    <div className='"max-w-[480px]"'>
+      {/* need app bar variants */}
+      {appbar ?? <div className="h-16" />}
+      <main className="px-5 pt-4">{children}</main>
+    </div>
+  );
+};
