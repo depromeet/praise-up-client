e026d5e4094470caf6f1030ce52a6111af2ce328
Chaemin-L <chaem12@kookmin.ac.kr>
2023-12-26 22:15:44 +0900

    Chaemin-L | feat: 오늘의 게시글 작성 버튼 구현

---
+++ src/app/main/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import GotoWrite from "./goToWrite";
+
+import { DefaultLayout } from "@/components/layout/default";
+
+export const Main = () => {
+  return (
+    <DefaultLayout>
+      <GotoWrite />
+    </DefaultLayout>
+  );
+};
