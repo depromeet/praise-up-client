419cf2f507a858bf399630ff43d59705be27416b
klmhyeonwooo <gentlemonster77@naver.com>
2023-12-28 02:32:31 +0900

    fix: modify common singleButton scalability

--- src/components/common/SingleButton.tsx
+++ src/components/common/SingleButton.tsx
@@ -1,15 +1,14 @@
-interface ButtonProps {
+type ButtonProps = {
   text: string;
-  onClick?: React.MouseEventHandler<HTMLButtonElement>;
-}
+} & Omit<React.ButtonHTMLAttributes<HTMLButtonElement>, "type">;
 
-export const SingleButton = ({ text, onClick }: ButtonProps) => {
+export const SingleButton = ({ text, ...props }: ButtonProps) => {
   return (
     <div className="w-360px h-98px px-20px pb-32px pt-12px">
       <button
         type="button"
         className="flex h-54px w-full items-center justify-center rounded-2 border bg-[#242B37] py-16px font-semibold text-white"
-        onClick={onClick}
+        {...props}
       >
         {text}
       </button>
