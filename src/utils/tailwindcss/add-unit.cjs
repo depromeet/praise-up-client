9d65c72d6fd21e1b1c6d501a812cdfd6aee8e392
2-NOW <kj109888@gmail.com>
2023-12-27 12:55:29 +0900

    feat: update addUnit start numberic value

--- src/utils/tailwindcss/add-unit.cjs
+++ src/utils/tailwindcss/add-unit.cjs
@@ -1,4 +1,4 @@
-function addUnit(to = 400, multiple = 4, unit = "px") {
+function addUnit(to = 200, multiple = 4, unit = "px") {
   return Array.from(Array(to)).reduce((acc, _, i) => {
     const value = `${Number(i) * multiple}${unit}`;
     return { ...acc, [value]: value };
