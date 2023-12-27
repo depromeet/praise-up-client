692c9ea47f18b44b267a6c36d1eb06bced6e149e
klmhyeonwooo <gentlemonster77@naver.com>
2023-12-28 00:48:39 +0900

    fix: modify switch pixel unit

--- src/components/layout/default.tsx
+++ src/components/layout/default.tsx
@@ -8,8 +8,8 @@ export const DefaultLayout = ({ appbar, children }: Props) => {
   return (
     <div className="absolute left-1/2 top-1/2 h-full w-full max-w-[480px] -translate-x-1/2 -translate-y-1/2 border-x ">
       {/* need app bar variants */}
-      {appbar ?? <div className="h-16" />}
-      <div className="px-5 pt-4">{children}</div>
+      {appbar ?? <div className="h-64px" />}
+      <div className="px-20px pt-16px">{children}</div>
     </div>
   );
 };
