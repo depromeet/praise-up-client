408c5a71dfb8064bea91fc3b553483d80f9c59b6
Jungjjeong <sky990106@dgu.ac.kr>
2023-12-27 22:41:58 +0900

    jungjjeong | refactor: preview background image 교체 및 구조 분리

--- src/app/archive/index.tsx
+++ src/app/archive/index.tsx
@@ -1,26 +1,11 @@
-import { MarbleCard } from "@/components/app/marbleCard";
+import { Preview } from "./preview";
+
 import { DefaultLayout } from "@/components/layout/default";
-import { Arrow } from "@/icons/arrow";
-import { MarbleBg } from "@/icons/marbleBg";
 
 export const Archive = () => {
   return (
     <DefaultLayout>
-      <div className="flex flex-col gap-9">
-        <p className="text-xl font-semibold text-gray-800">
-          공개 예정 칭찬게시물
-        </p>
-        <MarbleCard />
-      </div>
-      <div className="absolute bottom-0 left-0 h-[184px] w-full bg-gradient-to-b from-white to-cyan-200">
-        {/* TODO: background image <MarbleBg /> */}
-        <div className="flex h-full w-full flex-col items-center justify-center gap-3">
-          <div className="w-fit text-center text-sm font-semibold text-slate-500">
-            스크롤해서 칭찬 확인하기
-          </div>
-          <Arrow />
-        </div>
-      </div>
+      <Preview />
     </DefaultLayout>
   );
 };
--- src/app/archive/preview.tsx
+++ src/app/archive/preview.tsx
@@ -1,3 +1,23 @@
+import { MarbleCard } from "@/components/app/marbleCard";
+import { Arrow } from "@/icons/arrow";
+
 export const Preview = () => {
-  return <div>Preview Area</div>;
+  return (
+    <>
+      <div className="flex flex-col gap-9">
+        <p className="text-xl font-semibold text-gray-800">
+          공개 예정 칭찬게시물
+        </p>
+        <MarbleCard />
+      </div>
+      <div className="absolute bottom-0 left-0 h-[184px] w-full bg-[url('/src/assets/marble_bg.png')] bg-cover">
+        <div className="flex h-full w-full flex-col items-center justify-center gap-3">
+          <div className="w-fit text-center text-sm font-semibold text-slate-500">
+            스크롤해서 칭찬 확인하기
+          </div>
+          <Arrow />
+        </div>
+      </div>
+    </>
+  );
 };
