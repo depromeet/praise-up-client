1f0f2b998fb8eb7f200609e0a2916348e605b22c
klmhyeonwooo <gentlemonster77@naver.com>
2023-12-27 21:24:19 +0900

    fix : modify default layout

--- src/components/layout/default.tsx
+++ src/components/layout/default.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 export const DefaultLayout = ({ appbar, children }: Props) => {
   return (
-    <div className="max-w-[480px]">
+    <div className="absolute left-1/2 top-1/2 h-full w-full max-w-[480px] -translate-x-1/2 -translate-y-1/2 border-x ">
       {/* need app bar variants */}
       {appbar ?? <div className="h-16" />}
       <div className="px-5 pt-4">{children}</div>
