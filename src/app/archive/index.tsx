5ba2606d36c57eaf955bb516c3aeeec0151cc51b
Jungjjeong <sky990106@dgu.ac.kr>
2023-12-27 22:33:40 +0900

    jungjjeong | feat: preview page markup

--- src/app/archive/index.tsx
+++ src/app/archive/index.tsx
@@ -1,3 +1,26 @@
+import { MarbleCard } from "@/components/app/marbleCard";
+import { DefaultLayout } from "@/components/layout/default";
+import { Arrow } from "@/icons/arrow";
+import { MarbleBg } from "@/icons/marbleBg";
+
 export const Archive = () => {
-  return <div className="h-screen bg-white text-black">Archive Page</div>;
+  return (
+    <DefaultLayout>
+      <div className="flex flex-col gap-9">
+        <p className="text-xl font-semibold text-gray-800">
+          공개 예정 칭찬게시물
+        </p>
+        <MarbleCard />
+      </div>
+      <div className="absolute bottom-0 left-0 h-[184px] w-full bg-gradient-to-b from-white to-cyan-200">
+        {/* TODO: background image <MarbleBg /> */}
+        <div className="flex h-full w-full flex-col items-center justify-center gap-3">
+          <div className="w-fit text-center text-sm font-semibold text-slate-500">
+            스크롤해서 칭찬 확인하기
+          </div>
+          <Arrow />
+        </div>
+      </div>
+    </DefaultLayout>
+  );
 };
---
+++ src/components/app/marbleCard/index.tsx
@@ -0,0 +1,24 @@
+export const MarbleCard = () => {
+  return (
+    <div className="flex w-full flex-col gap-4 rounded-2xl bg-gray-100 p-4 pt-5">
+      <div className="flex flex-col gap-0.5 text-lg">
+        <p className="text-gray-500 ">지영님이 칭찬 받을</p>
+        <div>
+          <span className="font-semibold text-gray-700">센스있는</span>
+          <span className="text-gray-500"> 순간</span>
+        </div>
+      </div>
+      {/* background black은 임시 */}
+      <div className="relative box-border w-full rounded-xl bg-black after:block after:pb-[calc(100%)]">
+        <div className="absolute flex h-full w-full flex-col justify-end gap-2 p-[18px]">
+          <div className="text-white ">
+            한시간만에 뚝딱 완성한 나의 첫 요리 😆
+            <br />
+            간단한 요리지만 너무 뿌듯하다!
+          </div>
+          <p className="text-sm text-white ">23.12.09</p>
+        </div>
+      </div>
+    </div>
+  );
+};
