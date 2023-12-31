933fb890d06c0c0c168f55bcf40e9cb2fa7f31b2
Chaemin-L <chaem12@kookmin.ac.kr>
2023-12-31 21:03:47 +0900

    feat: scroll-smooth 속성 추가

--- src/components/layout/default.tsx
+++ src/components/layout/default.tsx
@@ -10,7 +10,7 @@ export const DefaultLayout = ({ isMain = false, appbar, children }: Props) => {
   return (
     <div
       className={clsx(
-        { "bg-gray-100 overflow-auto scrollbar-hide": isMain },
+        { "bg-gray-100 scroll-smooth overflow-auto scrollbar-hide": isMain },
         "absolute left-1/2 top-1/2 h-full w-full max-w-[480px] -translate-x-1/2 -translate-y-1/2 border-x ",
       )}
     >
