92e1e8fab678521ef807d7c395925ea7e8e8d243
Chaemin-L <chaem12@kookmin.ac.kr>
2024-01-03 08:39:25 +0900

    fix: 불필요한 속성 정리

--- src/app/unpublished-post/index.tsx
+++ src/app/unpublished-post/index.tsx
@@ -49,10 +49,10 @@ export const UnpublishedPostPage = () => {
         </div>
       </div>
 
-      <div className="fixed bottom-8 flex w-full -translate-x-5 justify-center px-5 pb-8 pt-3">
+      <div className="fixed bottom-0 left-0 flex w-full justify-center px-5 pb-8 pt-3">
         {/* TODO: 추후에 공통 버튼 컴포넌트로 변경 */}
         <button
-          className="text-b2-strong absolute rounded-2 bg-primary px-[52px] py-4 text-oncolor"
+          className="text-b2-strong rounded-2 bg-primary px-[52px] py-4 text-oncolor"
           onClick={() => console.log("링크가 복사됨")}
         >
           링크 공유하고 칭찬 받기
