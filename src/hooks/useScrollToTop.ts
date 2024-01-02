931ab18a97e4f40c95ec97f6f46b8e1a2e6f8c84
klmhyeonwooo <gentlemonster77@naver.com>
2024-01-03 01:38:43 +0900

    feat: useScrollToTop 공통 스크롤 초기화 훅 구현

---
+++ src/hooks/useScrollToTop.ts
@@ -0,0 +1,13 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+/** 페이지를 이동할 때, 이전 스크롤을 초기화하고 싶을 경우에 사용하는 스크롤 훅 */
+export function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
