5e3b1b62916db0018e82ed31058cffbbdb4321cd
Jungjjeong <sky990106@dgu.ac.kr>
2024-01-02 23:28:30 +0900

    jungjjeong | style: default layout 스타일 변경

--- src/components/layout/default.tsx
+++ src/components/layout/default.tsx
@@ -6,10 +6,10 @@ type Props = {
 
 export const DefaultLayout = ({ appbar, children }: Props) => {
   return (
-    <div className="absolute left-1/2 top-1/2 h-full w-full max-w-[480px] -translate-x-1/2 -translate-y-1/2 border-x ">
+    <div className="mx-auto flex h-screen w-screen max-w-[480px] flex-col border-x">
       {/* need app bar variants */}
       {appbar ?? <div className="h-64px" />}
-      <div className="px-20px pt-16px">{children}</div>
+      <div className="flex flex-1 flex-col px-20px pt-16px">{children}</div>
     </div>
   );
 };
