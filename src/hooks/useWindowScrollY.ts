62adadd2f65211bf16d457f8304ff0df96e20548
Jungjjeong <sky990106@dgu.ac.kr>
2024-01-02 23:26:38 +0900

    jungjjeong | feat: useWindowsScrollY hooks 구현

---
+++ src/hooks/useWindowScrollY.ts
@@ -0,0 +1,38 @@
+import _ from "lodash";
+import { useState, useEffect, useCallback, RefObject } from "react";
+
+interface IUseWindowScrollYProps {
+  point?: number;
+  scrollRef: RefObject<HTMLDivElement>;
+}
+
+const useWindowScrollY = ({ point = 0, scrollRef }: IUseWindowScrollYProps) => {
+  const [isDown, setIsDown] = useState(false);
+  const [scrollY, setScrollY] = useState(window.scrollY);
+  const [isOverflow, setIsOverflow] = useState(false);
+
+  const handleScroll = useCallback(() => {
+    const currentScrollY = scrollRef.current?.scrollTop || 0;
+
+    setScrollY((prevScrollY) => {
+      setIsDown(() => prevScrollY <= currentScrollY);
+      setIsOverflow(() => point < currentScrollY);
+      return currentScrollY;
+    });
+  }, [setScrollY, setIsDown, setIsOverflow, point, scrollRef]);
+
+  useEffect(() => {
+    scrollRef.current?.addEventListener("scroll", _.debounce(handleScroll, 50));
+
+    return () => {
+      scrollRef.current?.removeEventListener(
+        "scroll",
+        _.debounce(handleScroll, 50),
+      );
+    };
+  }, [handleScroll, scrollRef]);
+
+  return { isDown, isOverflow, scrollY };
+};
+
+export default useWindowScrollY;
