102789e1885dcafcf5c47666db01d4ffb3612669
Chaemin-L <chaem12@kookmin.ac.kr>
2024-01-03 19:33:18 +0900

    fix: 초기에 발생하는 타이머 delay 제거

--- src/hooks/useInterval.ts
+++ src/hooks/useInterval.ts
@@ -3,10 +3,20 @@ import { useEffect, useRef } from "react";
 export function useInterval(callback: () => void, delay: number | null) {
   const savedCallback = useRef<() => void>();
 
+  // for callback에서 사용하는 state을 최신으로 유지
   useEffect(() => {
     savedCallback.current = callback;
   }, [callback]);
 
+  // for callback이 delay없이 바로 동작
+  useEffect(() => {
+    const id = setTimeout(() => {
+      if (typeof savedCallback.current === "function") savedCallback.current();
+    });
+    return () => clearTimeout(id);
+  }, []);
+
+  // for 주기적으로 callback을 실행
   useEffect(() => {
     if (delay === null) return;
 
