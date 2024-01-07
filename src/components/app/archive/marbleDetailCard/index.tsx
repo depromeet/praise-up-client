6b63678980af26f5828007207c0c23dc21587a85
Jungjjeong <sky990106@dgu.ac.kr>
2024-01-07 17:00:44 +0900

    jungjjeong | style: marbleDetailCard 마크업 수정

--- src/components/app/archive/marbleDetailCard/index.tsx
+++ src/components/app/archive/marbleDetailCard/index.tsx
@@ -17,10 +17,9 @@ export const MarbleDetailCard = () => {
     console.log(result);
   };
 
+  // TODO: Add image save button
   return (
-    <div
-      className={`mx-[20px] flex flex-col justify-center gap-4 self-stretch rounded-2xl bg-marble-detail bg-cover px-4 pb-5 pt-4`}
-    >
+    <div className="mx-[20px] flex flex-col justify-center gap-4 self-stretch rounded-2xl bg-marble-detail bg-cover px-4 pb-5 pt-4 text-primary">
       <div className="relative box-border w-full rounded-xl bg-black after:block after:pb-[calc(100%)]">
         <button
           // eslint-disable-next-line @typescript-eslint/no-misused-promises
@@ -30,15 +29,15 @@ export const MarbleDetailCard = () => {
           <Overflow />
         </button>
       </div>
-      <div className="text-gray-800">
+      <p>
         오늘도 요리한 당신 정말 대단하다
         <br />
         앞으로도 그렇게 열심히 요리길만 걷기를
-      </div>
-      <div className="flex items-center justify-start gap-1 font-semibold">
-        <span className="text-gray-500">from.</span>
-        <span className="text-gray-800">태롱이</span>
-      </div>
+      </p>
+      <p className="flex items-center justify-start gap-1 font-semibold">
+        <span className="text-teritary">from.</span>
+        <span>태롱이</span>
+      </p>
     </div>
   );
 };
