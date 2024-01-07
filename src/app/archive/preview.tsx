7fafd38642795a9916cdd810a0d8686111f85d3f
Jungjjeong <sky990106@dgu.ac.kr>
2024-01-07 16:23:34 +0900

    jungjjeong | style: priview page 마크업 수정

--- src/app/archive/preview.tsx
+++ src/app/archive/preview.tsx
@@ -6,14 +6,12 @@ export const Preview = () => {
   return (
     <DefaultLayout>
       <div className="flex flex-col gap-9">
-        <p className="text-xl font-semibold text-gray-800">
-          공개 예정 칭찬게시물
-        </p>
+        <p className="text-xl font-semibold text-primary">나의 칭찬게시물</p>
         <MarbleCard />
       </div>
       <div className="absolute bottom-0 left-0 h-184px w-full bg-marble-preview bg-cover">
         <div className="flex h-full w-full flex-col items-center justify-center gap-3">
-          <div className="w-fit text-center text-sm font-semibold text-slate-500">
+          <div className="w-fit text-sm font-semibold text-slate-600">
             스크롤해서 칭찬 확인하기
           </div>
           <Arrow />
--- src/components/app/archive/marbleCard/index.tsx
+++ src/components/app/archive/marbleCard/index.tsx
@@ -3,12 +3,12 @@ import clsx from "clsx";
 export const MarbleCard = () => {
   return (
     <div className="flex w-full flex-col gap-4 rounded-2xl bg-gray-100 p-4 pt-5">
-      <div className="flex flex-col gap-0.5 text-lg">
-        <p className="text-gray-500 ">지영님이 칭찬 받을</p>
-        <div>
-          <span className="font-semibold text-gray-700">센스있는</span>
-          <span className="text-gray-500"> 순간</span>
-        </div>
+      <div className="flex flex-col gap-0.5 text-lg text-gray-700">
+        <p>지영님이 칭찬 받을</p>
+        <p>
+          <span className="font-semibold text-gray-800">센스있는</span>
+          <span> 순간</span>
+        </p>
       </div>
       {/* background black은 임시 */}
       <div
@@ -17,13 +17,13 @@ export const MarbleCard = () => {
           "relative box-border w-full rounded-xl bg-black",
         )}
       >
-        <div className="absolute flex h-full w-full flex-col justify-end gap-2 p-18px">
-          <div className="text-white ">
+        <div className="absolute flex h-full w-full flex-col justify-end gap-2 p-18px text-white">
+          <p>
             한시간만에 뚝딱 완성한 나의 첫 요리 😆
             <br />
             간단한 요리지만 너무 뿌듯하다!
-          </div>
-          <p className="text-sm text-white ">23.12.09</p>
+          </p>
+          <p className="text-sm">23.12.09</p>
         </div>
       </div>
     </div>
