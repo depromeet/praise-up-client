1a7dcb046397253e28fd6738f38319cbe74d0b81
Chaemin-L <chaem12@kookmin.ac.kr>
2024-01-08 02:19:16 +0900

    refactor: 사파리 환경 고려 layered background 재구현

--- src/components/app/add-comment/layered-background.tsx
+++ src/components/app/add-comment/layered-background.tsx
@@ -1,38 +1,22 @@
 import { PropsWithChildren } from "react";
 
-import bg_blur from "@/assets/imgs/bg_blur.svg";
 import bg_circles from "@/assets/imgs/bg_circles.svg";
-
-/* 
-  일러스트 img [abs] > 흰 배경 img (opacity:75%) [abs] > body [relative]
-  위 순서대로 레이어드 배경을 구성합니다
-*/
 
 export const LayeredBackground = ({ children }: PropsWithChildren) => {
   return (
-    <div className="h-full w-full">
-      {/* circle 일러스트 img */}
-      <div className="absolute left-0  h-full w-full overflow-x-hidden">
-        <div
-          className="left-0 h-full w-full blur-[50px]"
-          style={{
-            backgroundImage: `url(${bg_circles})`,
-            backgroundSize: "cover",
-            backgroundRepeat: "no-repeat",
-            backgroundPositionY: "69px",
-          }}
-        />
-      </div>
-      {/* 흰 배경 img (opacity: 75%) */}
+    <div className="overflow-hidden">
       <div
-        className="absolute left-0 h-full w-full opacity-75"
+        className="absolute left-0 h-full w-full overflow-hidden blur-[50px] after:absolute after:left-0 after:h-full after:w-full after:bg-white after:opacity-75"
         style={{
-          background: `url(${bg_blur})`,
-          backgroundSize: "contain",
+          backgroundImage: `url(${bg_circles})`,
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          backgroundPositionY: "88px",
+          backgroundAttachment: "scroll",
         }}
-      />
-      {/* body */}
-      <div className="relative flex h-full flex-col gap-9 pb-[60px]">
+      ></div>
+
+      <div className="relative flex h-full w-full flex-col gap-9 pb-[60px]">
         {children}
       </div>
     </div>
