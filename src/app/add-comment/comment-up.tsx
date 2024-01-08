eb5169f2d2043264acf3e8e4bd13f35a44c16124
Chaemin-L <chaem12@kookmin.ac.kr>
2024-01-09 00:25:21 +0900

    feat: 애니메이션을 고려한 화면 전환 딜레이 추가

--- src/app/add-comment/comment-up.tsx
+++ src/app/add-comment/comment-up.tsx
@@ -1,10 +1,35 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 import BgCirclesSVG from "@/assets/imgs/marbles.svg?react";
 import { DraggableMarble } from "@/components/app/add-comment/draggable-marble";
 import { LayeredBackground } from "@/components/app/add-comment/layered-background";
 import { AnimatedArrow } from "@/components/common/animated-arrow";
+import { Header } from "@/components/common/header";
 import { DefaultLayout } from "@/components/layout/default";
 
+const FLY_DURATION = 1; // second
+
 export const CommentUpPage = () => {
+  const [isReached, setIsReached] = useState<boolean>(false);
+
+  const navigate = useNavigate();
+
+  // navigation after animation duration
+  useEffect(() => {
+    if (isReached) {
+      void handleMove();
+    }
+  }, [isReached]);
+
+  const handleMove = async () => {
+    await new Promise(() =>
+      setTimeout(() => {
+        navigate("/clap/done");
+      }, FLY_DURATION * 1000),
+    );
+  };
+
   return (
     <DefaultLayout>
       <LayeredBackground>
@@ -12,12 +37,17 @@ export const CommentUpPage = () => {
         <div className="mt-[32px] flex h-full w-full flex-col items-center justify-center gap-[46px]">
           <div className="flex flex-col items-center justify-center gap-[50px]">
             <AnimatedArrow />
-            <DraggableMarble nickname="쥐렁이" />
+            <DraggableMarble
+              isReached={isReached}
+              setIsReached={setIsReached}
+              flyDuration={FLY_DURATION}
+              nickname="쥐렁이"
+            />
           </div>
-          <div className="text-h3 flex flex-col items-center gap-1.5 text-primary">
-            <h3>구슬을 위로 밀어서</h3>
-            <h3>칭찬구슬을 전달하세요</h3>
-          </div>
+          <Header
+            className="text-center"
+            text={`{구슬을 위로 밀어서}\\n{칭찬 구슬을 전달하세요!}`}
+          />
         </div>
       </LayeredBackground>
     </DefaultLayout>
--- src/components/app/add-comment/draggable-marble.tsx
+++ src/components/app/add-comment/draggable-marble.tsx
@@ -1,17 +1,36 @@
-import { useState, MouseEvent, TouchEvent } from "react";
+import {
+  useState,
+  MouseEvent,
+  TouchEvent,
+  Dispatch,
+  SetStateAction,
+} from "react";
 
 import MarbleLargeSVG from "@/assets/imgs/marble_lg.svg?react";
 
 interface DraggableMarbleProps {
-  nickname: string;
+  isReached: boolean; // when target distance is reached
+  setIsReached: Dispatch<SetStateAction<boolean>>;
+  flyDuration?: number; // sec
+  nickname: string; // nickname on marble
 }
 
 // 구슬이 원위치로 돌아가지 않는 최소 이동거리
 const MIN_DISTANCE = 200;
 
-export const DraggableMarble = ({ nickname }: DraggableMarbleProps) => {
+export const DraggableMarble = ({
+  isReached,
+  setIsReached,
+  flyDuration = 1,
+  nickname,
+}: DraggableMarbleProps) => {
   const [posY, setPosY] = useState(0);
   const [touchedY, setTouchedY] = useState(0);
+
+  const FADEOUT_STYLE = {
+    transition: `all ${flyDuration}s cubic-bezier(0.4, 0, 1, 1) `,
+    transform: "translateY(-800px)",
+  };
 
   const onMouseDown = (mouseDownEvent: MouseEvent<HTMLElement>) => {
     const mouseMoveHandler = (moveEvent: MouseEvent) => {
@@ -21,11 +40,9 @@ export const DraggableMarble = ({ nickname }: DraggableMarbleProps) => {
     };
 
     const mouseUpHandler = (moveEvent: MouseEvent<HTMLElement>) => {
-      console.log(mouseDownEvent.pageY, moveEvent.pageY);
+      if (mouseDownEvent.pageY - moveEvent.pageY < MIN_DISTANCE) setPosY(0);
+      else setIsReached(true);
 
-      if (mouseDownEvent.pageY - moveEvent.pageY < MIN_DISTANCE) {
-        setPosY(0);
-      }
       document.removeEventListener("mousemove", mouseMoveHandler);
     };
 
@@ -43,14 +60,14 @@ export const DraggableMarble = ({ nickname }: DraggableMarbleProps) => {
     };
 
     const onTouchEnd = (moveEvent: TouchEvent) => {
-      console.log(moveEvent.changedTouches[0].screenY);
       if (
         touchStartEvent.changedTouches[0].pageY -
           moveEvent.changedTouches[0].pageY <
         MIN_DISTANCE
-      ) {
+      )
         setTouchedY(0);
-      }
+      else setIsReached(true);
+
       document.removeEventListener("touchmove", onTouchMove);
     };
 
@@ -64,6 +81,7 @@ export const DraggableMarble = ({ nickname }: DraggableMarbleProps) => {
       style={{
         transform: `translateY(${posY | touchedY}px)`,
         transition: "ease-in-out",
+        ...(isReached ? FADEOUT_STYLE : {}),
       }}
       onMouseDown={onMouseDown} //desktop
       onTouchStart={onTouchStart} //mobile
