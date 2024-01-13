a8835c7747074cf48937c343fec94b447b08e8db
klmhyeonwooo <gentlemonster77@naver.com>
2024-01-14 00:30:45 +0900

    fix: 모바일 환경 대응을 위한 단위 변경

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,21 @@
 import "@/style/tailwindcss.css";
 import "@/style/global.css";
 
+import { AnimatePresence } from "framer-motion";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { ErrorBoundary } from "react-error-boundary";
+
+import { NotFound } from "./app/error/404";
 
 import { Routers } from "@/routes";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Routers />
+    <ErrorBoundary fallback={<NotFound />}>
+      <AnimatePresence>
+        <Routers />
+      </AnimatePresence>
+    </ErrorBoundary>
   </StrictMode>,
 );
