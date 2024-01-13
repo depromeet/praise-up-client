a1cd1b9d3b7647727725b42c30612ed874832dd5
Chaemin-L <chaem12@kookmin.ac.kr>
2024-01-13 23:24:37 +0900

    feat: 오늘의 게시물 empty case 구현

--- src/app/home/to-be-opened.tsx
+++ src/app/home/to-be-opened.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 
 import { CardSwiper } from "@/components/app/home/card-swiper";
+import { EmptyCard } from "@/components/app/home/empty-card";
 import { RecentCard } from "@/components/app/home/recent-card";
 import tempData from "@/data/main-temp-data.json";
 
@@ -19,6 +20,7 @@ export const ToBeOpened = () => {
   const [data, setData] = useState<Array<DataType>>([]);
   const date1 = new Date();
   const date2 = new Date();
+  const [isEmpty, setIsEmpty] = useState(true);
 
   // test data: before & after date
   const testDate = [
@@ -27,7 +29,8 @@ export const ToBeOpened = () => {
   ];
 
   useEffect(() => {
-    if (!tempData.data.length) return;
+    if (!isEmpty) return;
+    console.log(tempData.data);
     setData(
       tempData.data.map((data: InputDataType, i: number) => {
         return { ...data, openDatetime: testDate[i] };
@@ -35,10 +38,20 @@ export const ToBeOpened = () => {
     );
   }, []);
 
+  useEffect(() => {
+    setIsEmpty(data.length === 0);
+  }, [data]);
+
   return (
     <div className="mb-4 flex flex-col gap-5">
       <h2 className="text-h2 text-gray-900">공개 예정 칭찬게시물</h2>
 
+      {isEmpty && (
+        <EmptyCard
+          text="오늘의 게시물을 작성하지 않았어요"
+          subText="상단의 버튼을 눌러 게시물을 작성해보세요"
+        />
+      )}
       <CardSwiper>
         {data?.map((data, i) => <RecentCard key={i} {...data} />)}
       </CardSwiper>
---
+++ src/components/app/home/empty-card.tsx
@@ -0,0 +1,18 @@
+import { EmptyArchiveSVG } from "@/assets/imgs/empty-archive";
+
+interface EmptyCardProps {
+  text: string;
+  subText: string;
+}
+
+export const EmptyCard = ({ text, subText }: EmptyCardProps) => {
+  return (
+    <div className="flex w-full flex-col items-center justify-center gap-2.5 py-[76px]">
+      <EmptyArchiveSVG />
+      <div className="flex flex-col items-center gap-1">
+        <span className="text-b2-strong">{text}</span>
+        <span className="text-b3-compact">{subText}</span>
+      </div>
+    </div>
+  );
+};
