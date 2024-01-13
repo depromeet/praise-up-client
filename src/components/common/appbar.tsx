bb3cf415460e91f1fd412655e14a17a28bd15730
2-NOW <kj109888@gmail.com>
2024-01-14 02:45:55 +0900

    feat: add appbar with variants

---
+++ src/components/common/appbar.tsx
@@ -0,0 +1,39 @@
+import { ReactNode } from "react";
+
+import { LogoSVG } from "@/assets/icons/logo";
+
+type LeftAndContent =
+  | {
+      left: ReactNode;
+      content?: string;
+    }
+  | {
+      left?: undefined;
+      content?: never;
+    };
+
+export const Appbar = ({
+  left,
+  right,
+  content,
+}: {
+  right?: ReactNode;
+} & LeftAndContent) => {
+  // if left is not defined, render logo
+  const _left = left ? left : <LogoSVG />;
+  const _content = left ? content : null;
+
+  return (
+    <div className="flex h-64px w-full items-center justify-between px-20px py-10px">
+      <div className="flex-1">
+        <span className="grid place-content-start">{_left}</span>
+      </div>
+      {_content && <div className="flex-none">{_content}</div>}
+      {right && (
+        <div className="flex-1">
+          <span className="grid place-content-end">{right}</span>
+        </div>
+      )}
+    </div>
+  );
+};
--- src/components/layout/home-layout.tsx
+++ src/components/layout/home-layout.tsx
@@ -1,17 +1,13 @@
 import { PropsWithChildren } from "react";
 
-import { LogoSVG } from "@/assets/icons/logo";
 import { UserSVG } from "@/assets/icons/user";
+import { Appbar } from "@/components/common/appbar";
 
 export const HomeLayout = ({ children }: PropsWithChildren) => {
   return (
     <div className=" relative left-1/2 top-1/2 h-full w-full max-w-[480px] -translate-x-1/2 select-none overflow-auto scroll-smooth border-x bg-gray-100 scrollbar-hide">
       <div className="px-20px pt-16px">
-        {/* 임시 앱바 */}
-        <div className="flex h-64px justify-between px-5 py-2.5">
-          <LogoSVG />
-          <UserSVG />
-        </div>
+        <Appbar right={<UserSVG />} />
         {children}
       </div>
     </div>
