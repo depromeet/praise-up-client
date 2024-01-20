e096f92c6e686f4d8750e48466e733d6c63fc8a4
Chaemin-L <chaem12@kookmin.ac.kr>
2024-01-21 03:09:00 +0900

    chore: 잘못된 style 제거

--- src/components/common/blured-appbar.tsx
+++ src/components/common/blured-appbar.tsx
@@ -7,10 +7,9 @@ interface BluredAppbar {
 
 export const BluredAppbar = ({ left, title }: BluredAppbar) => {
   return (
-    <div className="sticky left-0 top-0 z-30 flex h-64px items-center justify-center bg-oncolor/80 after:absolute after:z-20 after:h-64px after:w-full after:backdrop-blur-[10px]">
+    <div className="sticky left-0 top-0 z-30 flex h-64px w-full items-center justify-center bg-oncolor/80 after:absolute after:z-20 after:h-64px after:w-full after:backdrop-blur-[10px]">
       <div className="absolute left-0 z-30 ml-5">{left}</div>
       <span className="text-h4 z-30 text-primary">{title}</span>
-      <div className="w-6" />
     </div>
   );
 };
