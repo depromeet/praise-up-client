b609b67ad2f55a49a45876b2faac52ff293ce1cc
Chaemin-L <chaem12@kookmin.ac.kr>
2024-01-21 14:51:54 +0900

    style: 파일 및 컴포넌트명 변경

--- src/components/common/blurred-appbar.tsx
+++ src/components/common/blurred-appbar.tsx
@@ -1,11 +1,11 @@
 import { ReactNode } from "react";
 
-interface BluredAppbar {
+interface BlurredAppbar {
   left: ReactNode;
   title: string;
 }
 
-export const BluredAppbar = ({ left, title }: BluredAppbar) => {
+export const BlurredAppbar = ({ left, title }: BlurredAppbar) => {
   return (
     <div className="sticky left-0 top-0 z-30 flex h-64px w-full items-center justify-center bg-oncolor/80 after:absolute after:z-20 after:h-64px after:w-full after:backdrop-blur-[10px]">
       <div className="absolute left-0 z-30 ml-5">{left}</div>
