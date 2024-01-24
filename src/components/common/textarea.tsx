2cd9fc61b204eee224a508f72379668ee433b7ea
Jungjjeong <sky990106@dgu.ac.kr>
2024-01-25 01:58:45 +0900

    jungjjeong | fix: textarea style 수정

--- src/components/common/textarea.tsx
+++ src/components/common/textarea.tsx
@@ -16,7 +16,7 @@ export const Textarea = ({
 
   return (
     <Fragment>
-      <div className="flex h-102px w-full flex-col rounded-4 bg-white px-18px py-16px outline outline-[#E0E2E6] focus-within:outline focus-within:outline-1 focus-within:outline-[#338AFF]">
+      <div className="flex h-102px w-full flex-col rounded-4 bg-white px-18px py-16px outline outline-1 outline-[#E0E2E6] focus-within:outline focus-within:outline-1 focus-within:outline-[#338AFF]">
         <textarea
           className="text-b3-compact h-50px w-full resize-none bg-transparent caret-[#338AFF] placeholder:text-gray-600 focus:outline-none"
           title="텍스트 입력"
