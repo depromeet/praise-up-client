3cdeb867523e9426f46887c07dabe807ac8f1eb7
Jungjjeong <sky990106@dgu.ac.kr>
2024-01-25 00:50:27 +0900

    jungjjeong | style: placeholder style 변경

--- src/components/common/textarea.tsx
+++ src/components/common/textarea.tsx
@@ -18,7 +18,7 @@ export const Textarea = ({
     <Fragment>
       <div className="flex h-102px w-full flex-col rounded-4 bg-white px-18px py-16px outline outline-[#E0E2E6] focus-within:outline focus-within:outline-1 focus-within:outline-[#338AFF]">
         <textarea
-          className="text-b3-compact h-50px w-full resize-none bg-transparent caret-[#338AFF] focus:outline-none"
+          className="text-b3-compact h-50px w-full resize-none bg-transparent caret-[#338AFF] placeholder:text-gray-600 focus:outline-none"
           title="텍스트 입력"
           maxLength={LIMIT}
           onChange={(event) => {
