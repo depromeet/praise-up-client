17c9b98610303d2f3dd92a78890f55c3ac820bbc
Chaemin-L <chaem12@kookmin.ac.kr>
2024-01-25 10:43:28 +0900

    fix: card-swiper의 다음 card가 일부 보이도록 스타일 수정

--- src/components/layout/home-layout.tsx
+++ src/components/layout/home-layout.tsx
@@ -5,7 +5,7 @@ import { Appbar } from "@/components/common/appbar";
 
 export const HomeLayout = ({ children }: PropsWithChildren) => {
   return (
-    <div className=" relative left-1/2 top-1/2 h-full w-full max-w-[480px] -translate-x-1/2 select-none overflow-auto scroll-smooth border-x bg-gray-100 scrollbar-hide">
+    <div className=" relative left-1/2 top-1/2 h-full w-full max-w-[480px] -translate-x-1/2 select-none overflow-x-auto scroll-smooth border-x bg-gray-100 scrollbar-hide">
       <Appbar right={<UserSVG />} />
       <div className="px-20px pt-16px">{children}</div>
     </div>
