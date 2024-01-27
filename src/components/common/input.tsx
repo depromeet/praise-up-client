598251055d338b28f486127e69453b2a63f3d62b
Jungjjeong <sky990106@dgu.ac.kr>
2024-01-27 14:57:51 +0900

    jungjjeong | feat: input caret 색상 수정

--- src/components/common/input.tsx
+++ src/components/common/input.tsx
@@ -9,7 +9,7 @@ interface TextInputProps
 
 export const Input = ({ limit, value, onChange, ...props }: TextInputProps) => {
   return (
-    <div className=" flex items-center justify-between rounded-3 border border-gray-300 bg-white px-[18px] py-4 focus-within:border-blue-500">
+    <div className=" flex items-center justify-between rounded-3 border border-gray-300 bg-white px-[18px] py-4 caret-[#338AFF] focus-within:border-[#338AFF]">
       <input
         className="text-b3-compact w-full overflow-hidden text-ellipsis bg-transparent focus:outline-none"
         type="text"
