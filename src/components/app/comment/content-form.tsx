e9697c5f29ebbbbcfe15ef5aced6f11d61b922d0
Chaemin-L <chaem12@kookmin.ac.kr>
2024-01-28 20:41:36 +0900

    fix: 칭찬 메시지 관련 아이콘 변경 및 placeholder 설정

--- src/components/app/comment/content-form.tsx
+++ src/components/app/comment/content-form.tsx
@@ -16,14 +16,15 @@ export const ContentForm = ({ content, setContent }: ContentFormProps) => {
           limit={40}
           value={content}
           currentLength={content.length}
+          placeholder="칭찬 받을 순간에 대한 내용을 작성해주세요 (선택)"
           onChange={(e: ChangeEvent<HTMLTextAreaElement>) =>
             setContent(e.target.value)
           }
         />
       </div>
-      <div className="flex gap-[7px] break-keep">
+      <div className="flex gap-[4px] ">
         <InformationSVG />
-        <span className="text-cap-compact text-gray-600">
+        <span className="text-b3-long text-gray-600">
           욕설, 비속어나 부적절한 단어의 경우 서비스 사용이 제한되거나 관리자에
           의해 삭제될 수 있어요.
         </span>
