3f7db28489c34418434b1a609af9c37806c226d5
klmhyeonwooo <gentlemonster77@naver.com>
2024-01-29 19:06:52 +0900

    feat: 5차 OG 태그 반영

--- src/app/on-boarding-clap/index.tsx
+++ src/app/on-boarding-clap/index.tsx
@@ -1,5 +1,5 @@
 import { Fragment, useState } from "react";
-import { Helmet } from "react-helmet";
+import { Helmet } from "react-helmet-async";
 import { useNavigate } from "react-router-dom";
 
 import { NotFound } from "@/app/error/404";
