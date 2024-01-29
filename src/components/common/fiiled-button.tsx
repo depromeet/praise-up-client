3f6d6c11035bc5950db172ad850aa8ae81a92bfc
Jungjjeong <sky990106@dgu.ac.kr>
2024-01-29 21:01:07 +0900

    jungjjeong | style: button style 수정

--- src/components/common/fiiled-button.tsx
+++ src/components/common/fiiled-button.tsx
@@ -21,11 +21,12 @@ export const FilledButton = ({
     <button
       type="button"
       className={clsx(
-        "flex h-54px w-full items-center justify-center rounded-2 py-16px transition-all duration-300 disabled:bg-gray-400 disabled:text-gray-500",
+        "flex w-full items-center justify-center rounded-2 transition-all duration-300 disabled:bg-gray-400 disabled:text-gray-500",
         className,
         {
-          "text-b2-strong bg-[#242B37] text-white": colorSchema === "primary",
-          "text-b3-strong h-40px bg-transparent text-blue-500":
+          "text-b2-strong bg-[#242B37] text-white py-16px":
+            colorSchema === "primary",
+          "text-b3-strong h-40px bg-transparent text-blue-500 py-10px":
             colorSchema === "white",
           "rounded-none": isFull && isFullStyle,
         },
