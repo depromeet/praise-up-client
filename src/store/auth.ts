fd2bc8e74bc8e2eeb1605165c4f9a02335bbcdfd
Jungjjeong <sky990106@dgu.ac.kr>
2024-01-31 21:29:26 +0900

    jungjjeong | refactor: 코드 정리

--- src/store/auth.ts
+++ src/store/auth.ts
@@ -23,7 +23,7 @@ export const useAuthStore = create<Store>((set) => {
       set(() => ({
         auth: {
           userId,
-          isLogin: !!userId,
+          isLogin: Boolean(userId),
         },
       })),
   };
