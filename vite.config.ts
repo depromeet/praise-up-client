24ea9de96e6d27263e0c4ebf48102cb0e1f6e4c7
klmhyeonwooo <gentlemonster77@naver.com>
2024-02-18 23:11:46 +0900

    feat: add Google Analytics

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,36 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
+import dotenv from "dotenv";
+import { VitePluginRadar } from "vite-plugin-radar";
 import path from "path";
+
+dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [
+    react(),
+    svgr(),
+    VitePluginRadar({
+      enableDev: true,
+      analytics: [
+        {
+          id: process.env.VITE_GA_TRACKING_ID,
+          config: {
+            cookie_domain: "auto",
+            cookie_expires: 63072000,
+            cookie_prefix: "none",
+            cookie_update: true,
+            cookie_flags: "",
+            send_page_view: true,
+            allow_google_signals: true,
+            allow_ad_personalization_signals: true,
+          },
+        },
+      ],
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
